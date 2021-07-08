9026e6cce994ebccbda11a4efe59fedca81d57b1
Samuel <samuel210@live.fr>
2021-07-08 21:28:27 +0200

    Login et token

---
+++ routers/authentication.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+const userModel = require('../models/user')
+const { createToken } = require('../utilities/token')
+var { decrypt } = require("../utilities/crypto");
+
+router.post("/", async(req, rep) => {
+    var user = await userModel.findOne({ email: req.body.email });
+
+    if (user) {
+        if (decrypt(user.password) == req.body.password) {
+            const token = createToken(user);
+            //rep.header("Authorization", token);
+            rep.status(200).send({ token: token });
+        } else rep.send({ result: "PasswordNotCorrect" });
+    } else rep.send({ result: "UserNotExist" });
+});
+
+module.exports = router
