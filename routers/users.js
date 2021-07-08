4faee7c3d3aed18248e06d34c2b1343cb2d8cb6d
Samuel <samuel210@live.fr>
2021-07-08 12:30:47 +0200

    Ajout controle de l'email si il existe un autre utilisateur avec l'adresse mail du put pour eviter apres put deux utilisateur avec la meme adresse mail

--- routers/users.js
+++ routers/users.js
@@ -33,23 +33,30 @@ router.get("/:id", async(request, response) => {
 /* https://api.monsuperhotel.com/users/59bfd752z*/
 router.put("/:id", async(request, response) => {
     let { body } = request;
-    var verif = await userModel.findOne({ email: body.email })
+    let dejaEmail = false;
     if (validate(body.email)) {
-        if (verif) response.json({ result: 'UserEmailAlreadyExist' })
-        else {
+        var users = await userModel.find({})
+        for (let i = 0; i < users.length; i++) {
+            if (users[i]._id != request.params.id) {
+                if (users[i].email == body.email) {
+                    dejaEmail = true;
+                }
+            }
+        }
+        if (dejaEmail == false) {
             body.password = encrypt(body.password);
             var user = await userModel.findOneAndUpdate({ _id: request.params.id },
                 body, { new: true }
             );
             response.json(user);
-        }
+        } else response.status(400).send({ result: "UserEmailAlreadyExist" })
     } else response.json({ result: "EmailNotValid" })
 
 });
 
 /* https://api.monsuperhotel.com/users/59bfd752z*/
 router.delete("/:id", async(request, response) => {
-    var u = await userModel.findOne({ _id: request.params.id })
+    var u = await userodel.findOne({ _id: request.params.id })
     if (u) {
         var user = await userModel.findOneAndRemove({ _id: request.params.id });
         response.status(200).send({ result: 'Success' });
